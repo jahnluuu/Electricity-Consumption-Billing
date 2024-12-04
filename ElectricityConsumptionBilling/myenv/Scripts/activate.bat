1855b42fbb8e6983305ab0923a83e644b0170db7
Claive Justin Barrientos <claivejustinbarrientos12@gmail.com>
2024-12-05 04:05:33 +0800

    Manage User Page

--- ElectricityConsumptionBilling/myenv/Scripts/activate.bat
+++ ElectricityConsumptionBilling/myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lupoj\OneDrive\Documents\GitHub\Electricity-Consumption-Billing\ElectricityConsumptionBilling\myenv
+set VIRTUAL_ENV=C:\Users\Admin\Downloads\ECB\Electricity-Consumption-Billing\ElectricityConsumptionBilling\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
